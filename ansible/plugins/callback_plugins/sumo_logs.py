a841ef2479666aacfd58c79cb8af7272c4f4b8ef
David Gadoury <gadouryd@gmail.com>
2015-06-15 15:51:49 -0400

    removed unneeded print statement

--- ansible/plugins/callback_plugins/sumo_logs.py
+++ ansible/plugins/callback_plugins/sumo_logs.py
@@ -112,10 +112,7 @@ class CallbackModule(object):
         # Check to see if we are processing a role
         if len(my_list) == 2:
             self.role = str.strip(my_list[0])
-        #    self.role = self.role.strip()
             self.task = str.strip(my_list[1])
-         #   self.task = self.task.strip()
-            print "ROLE ROLE ROLE %s" % self.role
 
             # Check to see if we are procesing a new role. If so, calculate the duration of the previous role
             if self.current is not None and self.current != self.role:
@@ -129,8 +126,6 @@ class CallbackModule(object):
         # We are now processing playbook level tasks
         else:
             self.task = str.strip(my_list[0])
-            #self.task = str.strip(my_list[0])
-            #self.task = self.task.strip()
             if self.current != "NULL":
                 self.stats[self.current] = time.time() - self.stats[self.current]
             self.current = "NULL"
